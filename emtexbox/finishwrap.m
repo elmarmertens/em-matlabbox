c0936d46754dbab41c524b4dc1f77fce68e76197
Elmar Mertens <em@elmarmertens.com>
2018-03-27 12:02:45 +0200

    updated toolbox (mainly emtexbox)
    Signed-off-by: Elmar Mertens <em@elmarmertens.com>

--- emtexbox/finishwrap.m
+++ emtexbox/finishwrap.m
@@ -1,14 +1,15 @@
 if exist('wrap', 'var') && ~isempty(wrap)
-   fprintf('\n');
-   toc
-   fprintf('\n');
-
-   if isunix
-       wrap = latexwrapper(wrap, 'compileDVI2PDF');
-   else
-       wrap = latexwrapper(wrap, 'close');
-   end
-   
-   fprintf('Wrap file %s finished in %s\n', wrap.name, wrap.dir)
-   fprintf('The current time is %s.\n', datestr(now))
+    fprintf('\n');
+    toc
+    fprintf('\n');
+    
+    wrap = latexwrapper(wrap, 'compileDVI2PDF');
+    %    if isunix
+    %        wrap = latexwrapper(wrap, 'compileDVI2PDF');
+    %    else
+    %        wrap = latexwrapper(wrap, 'close');
+    %    end
+    
+    fprintf('Wrap file %s finished in %s\n', wrap.name, wrap.dir)
+    fprintf('The current time is %s.\n', datestr(now))
 end
