2a46084a7329963f202dca123d580a43540ac8b8
Elmar Mertens <em@elmarmertens.com>
2018-11-09 10:55:43 +0100

    getparpoolsize: robust to par prefs
    Signed-off-by: Elmar Mertens <em@elmarmertens.com>

--- emtools/getparpoolsize.m
+++ emtools/getparpoolsize.m
@@ -1,6 +1,17 @@
 function poolsize = getparpoolsize()
+ 
 
-p = gcp('nocreate'); % If no pool, do not create new one.
+try 
+    p = gcp; % get current pool or try to create a new one *if* parallel preferences are set to automatically create a pool when needed.
+    % Please see the "Parallel Computing Toolbox" section of the matlab preferences to see whether a pool will be automatically created. 
+    % To open these preferences run "preferences('Parallel Computing Toolbox')" from the Matlab command line.
+    % If preferences for automatic pool creation are disabled, "gcp" will not attempt to create a new pool
+catch poolME
+    warning(poolME.identifier, 'There was a problem obtaining a pool of parallel workers; tryxing to work without one.\n The error message was:\t %s', ...
+        poolME.message)
+    p = gcp('nocreate'); % If no pool can be created, do not create new one.
+end
+ 
 if isempty(p)
     poolsize = 0;
 else
