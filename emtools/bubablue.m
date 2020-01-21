55a0b136704ba75fa1ce689e207031fdf898f8ca
Elmar Mertens <em@elmarmertens.com>
2020-01-21 19:09:25 +0100

    addded: bubablue.m
    Signed-off-by: Elmar Mertens <em@elmarmertens.com>

---
+++ emtools/bubablue.m
@@ -0,0 +1,14 @@
+function bb = bubablue()
+% BUBABLUE ... 
+%  
+%   ... 
+
+%% VERSION INFO 
+% AUTHOR    : Elmar Mertens 
+% $DATE     : 29-Jan-2019 12:03:58 $ 
+% $Revision : 1.00 $ 
+% DEVELOPED : 9.5.0.1033004 (R2018b) Update 2 
+% FILENAME  : bubablue.m 
+
+bb =  [0,98,161] / 255; % buba blue
+
