52ab0a443b3e7019b069e65fd17742b49bdbb611
Elmar Mertens <em@elmarmertens.com>
2020-05-10 13:20:55 +0200

    set localtemp to pwd
    Signed-off-by: Elmar Mertens <em@elmarmertens.com>

--- emtools/localtemp.m
+++ emtools/localtemp.m
@@ -2,7 +2,7 @@ function p = localtemp
 % LOCALSTORE designates a folder where all matlab store temp files
 % default: tmp folder in current working directory
 
-p = fullfile(pwd, 'tmp');
+p = pwd;
 
 
 if ~exist(p, 'dir')
