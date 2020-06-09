4132afbd976bb340e7d9c8659a1c79931fae4db0
Elmar Mertens <em@elmarmertens.com>
2020-06-09 21:59:18 +0200

    stochvolKSCsqrt with optimized smoothing sampler

---
+++ emgibbsbox/StochVolKSCcorrsqrt.m
@@ -0,0 +1,61 @@
+function [h, h0, hshock, kai2States] = StochVolKSCcorrsqrt(logy2, h, hVCVsqrt, Eh0, sqrtVh0, KSC, KSCt, Nsv, T, rndStream)
+% StochVolKSC performs a Gibbs updating step on a SV model and it works over 
+% Nsv independent SV residuals
+%
+% Uses Kim, Shephard and Chib normal mixtures
+%
+% USAGE:[h, h0, kai2States] = StochVolKSCcorr(logy2, h, hVCV, Eh0, Vh0, KSC, KSCt, Nsv, T, rndStream)
+%
+% multivariate case with correlated shocks and RW dynamics
+%
+% See also abcrDisturbanceSmoothingSampler1draw, getKSCvalues
+
+%   Coded by  Elmar Mertens, em@elmarmertens.com
+
+
+%% VERSION INFO
+% AUTHOR    : Elmar Mertens
+% $DATE     : 28-Aug-2009 12:07:01 $
+% $Revision : 1.00 $
+% DEVELOPED : 7.7.0.471 (R2008b)
+% FILENAME  : StochVolKSC.m.m
+
+if isscalar(Eh0)
+    Eh0 = repmat(Eh0, Nsv, 1);
+end
+if isscalar(sqrtVh0)
+    sqrtVh0 = sqrtVh0 * eye(Nsv);
+end
+
+%% CORRIGENDUM CHANGES ORDER OF GIBBS STEPS!
+
+%% draw mixture states
+% zdraws are standardized draws for each component of the normal mixture 
+% zdraws is thus Nsv x T x Nmixtures
+% zdraws      = bsxfun(@minus, logy2 - h, KSCt.mean) ./ KSCt.vol;
+zdraws      = (logy2 - h - KSCt.mean) ./ KSCt.vol;
+
+% construct CDF
+% factor of sqrt(2 * pi) can be ommitted for kernel
+pdfKernel           = KSCt.pdf ./ KSCt.vol .* exp(-.5 * zdraws.^2); 
+cdf                 = cumsum(pdfKernel, 3);                % integrate
+% cdf(:,:,1:end-1)    = bsxfun(@rdivide, cdf(:,:,1:end-1), cdf(:,:, end)); 
+cdf(:,:,1:end-1)    = cdf(:,:,1:end-1) ./ cdf(:,:, end); % using automatic expansion 
+cdf(:,:,end)        = 1;    % normalize
+
+% draw states
+% kai2States  = sum(bsxfun(@gt, rand(rndStream, Nsv, T), cdf), 3) + 1;
+kai2States  = sum(rand(rndStream, Nsv, T) > cdf, 3) + 1;
+obs         = logy2 - KSC.mean(kai2States);
+
+%% KSC State Space
+sqrtR = zeros(Nsv,Nsv,T);
+for n = 1 : Nsv
+    sqrtR(n,n,:) = KSC.vol(kai2States(n,:));
+end
+
+% note: for larger systems, smoothing sampler turns out to be more
+% efficient than Carter-Kohn
+[h, hshock, h0] = vectorRWsmoothingsampler1draw(obs, hVCVsqrt, sqrtR, Eh0, sqrtVh0, rndStream);
+
+
