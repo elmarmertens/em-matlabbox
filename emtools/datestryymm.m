eeb3177ccc90911ed4a9a6b5c3b9c46f6dcdda84
Elmar Mertens <em@elmarmertens.com>
2020-07-28 20:01:11 +0200

    new: datestryymm
    Signed-off-by: Elmar Mertens <em@elmarmertens.com>

---
+++ emtools/datestryymm.m
@@ -0,0 +1,16 @@
+function s = datestryymm(d)
+% DATESTRYYMM returns date number as string in format yyyy:mm 
+%  
+% USAGE: s = datestryymm(d)
+%   ... 
+
+%% VERSION INFO 
+% AUTHOR    : Elmar Mertens 
+% $DATE     : 28-Jul-2020 19:59:11 $ 
+% $Revision : 1.00 $ 
+% DEVELOPED : 9.8.0.1417392 (R2020a) Update 4 
+% FILENAME  : datestryymm.m 
+
+
+
+s = sprintf('%s:%s', datestr(d, 'yyyy'), datestr(d, 'mm'));
