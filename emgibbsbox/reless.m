23ec41b2518e4a4adc72bad456c55ce9aee5967d
Elmar Mertens <em@elmarmertens.com>
2021-01-17 12:48:58 +0100

    new: reless function
    Signed-off-by: Elmar Mertens <em@elmarmertens.com>

---
+++ emgibbsbox/reless.m
@@ -0,0 +1,14 @@
+function r = reless(w)
+% RELESS ... 
+%  
+%   ... 
+
+%% VERSION INFO 
+% AUTHOR    : Elmar Mertens 
+% $DATE     : 16-Jan-2021 22:11:58 $ 
+% $Revision : 1.00 $ 
+% DEVELOPED : 9.9.0.1538559 (R2020b) Update 3 
+% FILENAME  : reless.m 
+
+N = size(w,1);
+r = 1 ./ sum(w.^2,1) / N;
