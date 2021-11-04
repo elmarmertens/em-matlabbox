428053dcbacb4f194ad7932f00069804c4e87eb1
Elmar Mertens <em@elmarmertens.com>
2021-11-04 16:41:30 +0100

    dummyobs4BVAR: added shrinkage parameters
    Signed-off-by: Elmar Mertens <em@elmarmertens.com>

--- emeconometrics/dummyobs4BVAR.m
+++ emeconometrics/dummyobs4BVAR.m
@@ -1,35 +1,52 @@
-function [Ystar, Xstar] = bayesRegDummyObs(Ny, p, Nx, ndxExo, priorMean, dof0, SIGMA0)
+function [Ystar, Xstar] = dummyobs4BVAR(Ny, p, Nx, ndxExo, lambda, priorMean, dof0, SIGMA0)
 % BAYESREGDUMMYOBS constructs dummy opbs for Bayesian Regression with Conjugate Prior
-%  
+%
 % usage [Ystar, Xstar] = bayesRegDummyObs(Ny, Nx, ndxExo, dof0, SIGMA0)
-% 
+%
 % Note: ndxExo can be a constant intercept or other deterministic dummies
 %
 % defaults ndxExo=Nx, priorMean=0, dof=0 (SIGMA0 irrelevant)
 %
 % see also BVARjeffries, BVARdraws
 
-%% VERSION INFO 
-% AUTHOR    : Elmar Mertens 
-% $DATE     : 04-Nov-2021 15:40:03 $ 
-% $Revision : 1.00 $ 
-% DEVELOPED : 9.11.0.1769968 (R2021b) 
-% FILENAME  : bayesRegDummyObs.m 
+%% VERSION INFO
+% AUTHOR    : Elmar Mertens
+% $DATE     : 04-Nov-2021 15:40:03 $
+% $Revision : 1.00 $
+% DEVELOPED : 9.11.0.1769968 (R2021b)
+% FILENAME  : dummyobs4BVAR.m
 
 %% process arguments
-if nargin < 4 || isempty(ndxExo)
+
+if nargin < 4 || isempty(lambda)
+    % hyperparameters (in Sims notation)
+    if Ny >= 15
+        lambda1     = 20; % overall shrinkage, corresponds to 1/20=0.05 in CCM
+    else
+        lambda1     = 10; % overall shrinkage, corresponds to 1/10=0.1 in CCM
+    end
+    lambda2     = 2; % decay
+    lambda0     = 1/100;
+    % lambda = [lambda0 lambda1 lambda2];
+else
+    lambda0 = lambda(1);
+    lambda1 = lambda(2);
+    lambda2 = lambda(3);
+end
+
+if nargin < 5 || isempty(ndxExo)
     ndxExo = Nx;
 end
 
-if nargin < 5 || isempty(priorMean)
+if nargin < 6 || isempty(priorMean)
     priorMean = 0;
 end
 
-if nargin < 6 || isempty(dof0)
+if nargin < 7 || isempty(dof0)
     dof0 = 0;
 end
 
-if nargin < 7 || isempty(SIGMA0)
+if nargin < 8 || isempty(SIGMA0)
     SIGMA0 = eye(Ny);
 end
 
@@ -45,15 +62,6 @@ ndxLags = ~ismember(1 : Nx, ndxExo);
 
 %% construct dummy obs for Bayesian prior
 
-% hyperparameters (in Sims notation)
-if Ny >= 15
-    lambda1     = 20; % overall shrinkage, corresponds to 1/20=0.05 in CCM
-else
-    lambda1     = 10; % overall shrinkage, corresponds to 1/10=0.1 in CCM
-end    
-lambda2     = 2; % decay
-lambda0     = 1/100;
-
 % Set up dummy variables
 Xstar = [];
 Ystar = [];
@@ -67,7 +75,7 @@ if dof0 > 0
     thisYstar         = zeros(dof0, Ny);
     thisYstar(1:Ny,:) = sqrtSIGMA0 * dof0; % note: equivalent to stacking sqrtSIGMA0 dof0/Ny times, can also handle cases where dof0 is not a multiple of Ny
     Ystar = cat(1, Ystar, thisYstar);
-    Xstar = cat(1, Xstar, zeros(Ny * dof0, Nx));    
+    Xstar = cat(1, Xstar, zeros(Ny * dof0, Nx));
 end
 
 
