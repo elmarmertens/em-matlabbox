f764d0a143d23ccbc7124bed9ca708ddc6af778c
Elmar Mertens <em@elmarmertens.com>
2022-01-22 11:50:08 +0100

    commonSV code (with AR1, and precision-based sampling)

---
+++ emgibbsbox/CommonStochVolAR1.m
@@ -0,0 +1,52 @@
+function [h, hshock, kai2States] = CommonStochVolAR1(logy2, h, rho, hvol, KSC, KSCt, Ny, T, rndStream)
+% CommonStochVolAR1 performs a Gibbs updating step on a common SV model
+% with AR1 SV
+%
+% Uses Kim, Shephard and Chib normal mixtures
+%
+%
+% See also abcDisturbanceSmoothingSampler1draw, vectorRWsmoothingsampler1draw, getKSC7values, getKSC10values
+
+%   Coded by  Elmar Mertens, em@elmarmertens.com
+
+% note distinction between Ny observables, adn Nsv=1 SV processes
+
+
+%% draw mixture states
+% zdraws are standardized draws for each component of the normal mixture 
+% zdraws is thus Ny x T x Nmixtures
+zdraws      = (logy2 - h - KSCt.mean) ./ KSCt.vol;
+
+% construct CDF
+% factor of sqrt(2 * pi) can be ommitted for kernel
+pdfKernel           = KSCt.pdf ./ KSCt.vol .* exp(-.5 * zdraws.^2); 
+cdf                 = cumsum(pdfKernel, 3);                % integrate
+cdf(:,:,1:end-1)    = cdf(:,:,1:end-1) ./ cdf(:,:, end); % using automatic expansion 
+cdf(:,:,end)        = 1;    % normalize
+
+% draw states
+kai2States  = sum(rand(rndStream, Ny, T) > cdf, 3) + 1;
+
+
+%% KSC State Space
+obs   = logy2 - KSC.mean(kai2States);
+
+vecobs         = obs(:);
+noisevol       = KSC.vol(kai2States(:));
+[h, hshock]    = commonAR1noisePrecisionBasedSampler(vecobs, Ny, T, rho, hvol, noisevol, 1, rndStream);
+
+% DK sampler (quite a bit slower than precision-based sampler):
+% A     = rho;
+% B     = hvol;
+% C     = ones(Ny,1);
+% sqrtR = zeros(Ny,Ny,T);
+% for n = 1 : Ny
+%     sqrtR(n,n,:) = KSC.vol(kai2States(n,:));
+% end
+% sqrtVh0 = 0;
+% Eh0     = 0;
+% [h, hshock] = a2b2c2DisturbanceSmoothingSampler1draw(A, B, C, obs, Eh0, sqrtVh0, ...
+%     sqrtR, rndStream); 
+
+
+
--- emgibbsbox/commonAR1noisePrecisionBasedSampler.m
+++ emgibbsbox/commonAR1noisePrecisionBasedSampler.m
@@ -1,48 +1,52 @@
-function [Xdraw, Xhat, P] = rwnoisePrecisionBasedSampler(Y, Ny, T, volSTATE, volNOISE, X0, sqrtV0, Ndraws, rndStream)
+function [Xdraw, Xshock, Xhat, P] = commonAR1noisePrecisionBasedSampler(Y, Ny, T, rhoSTATE, volSTATE, volNOISE, Ndraws, rndStream)
 % abcdPrecisionBasedSampler computes smoothed kalman states using the stacked approach of 
 % Chan and Jeliazkov
 %  
 %   ... 
 
-% assumes volSTATE is Nx x 1 vector, and volNOISE is (Ny x T) x 1 vectors (i.e. no correlation within X and Y)
+% assumes rhoSTATE and volSTATE are scalars, and volNOISE is (Ny x T) x 1 vector (i.e. no correlation within X and Y)
+% X0 and sqrtV0 both zero, so that X0=0 is deterministic
 
-
-if nargin < 9
+if nargin < 7 || isempty(Ndraws)
     Ndraws = 1;
 end
-if nargin < 10
+if nargin < 8
     rndStream = getDefaultStream;
 end
 
 %% read parameters
 Y        = Y(:);
 NyT      = Ny * T;
-Nx       = Ny;
+Nx       = 1; % todo: hardcode
 NxT      = Nx * T; % obsolete but for better readability
 NxTm1    = Nx * (T - 1); % obsolete but for better readability
 
 %% construct stacked system
-XX0 = sparse(1:Nx, 1, X0, Nx * T, 1);
+XX0 = sparse(T, 1);
 
+% AA
 rowndx = [1 : NxT, Nx + 1 : NxT];
 colndx = [1 : NxT, 1 : NxTm1];
-values = [ones(1,NxT), -ones(1, NxTm1)];
-AA  = sparse(rowndx, colndx, values);
+values = [ones(1,NxT), -rhoSTATE * ones(1, T-1)];
+AA     = sparse(rowndx, colndx, values);
 
-CC  = speye(NyT, NyT);
+% CC
+rowndx = 1 : NyT;
+colndx = repmat(1:T, Ny, 1);
+colndx = colndx(:);
+values = ones(1, NyT);
+CC     = sparse(rowndx, colndx, values);
 
 
-% sqrtSIGMA = sparse(1:NxT, 1:NxT, [sqrtV0(:)', repmat(volSTATE(:)', 1, T-1)]);
-ITm1        = speye(T-1);
-sqrtSIGMA   = blkdiag(sqrtV0,  kron(ITm1, volSTATE));
 
-sqrtOMEGA   = sparse(1:NyT, 1:NyT, volNOISE);
+sqrtSIGMA   = volSTATE * speye(NxT); % Note: X(t=1) consists solely of innovation since X(t=0)=0
+sqrtOMEGA   = sparse(1:NyT, 1:NyT, volNOISE(:)');
 
 %% set up  stacked system
 
 
 AAtilde            = sqrtSIGMA \ AA;
-XX0tilde           = sqrtSIGMA \ XX0;
+XX0tilde           = XX0; % sqrtSIGMA \ XX0; since XX0=0
 
 CCtilde            = sqrtOMEGA \ CC;
 Ytilde             = sqrtOMEGA \ Y;
@@ -64,9 +68,15 @@ sqrtPXhat   = sqrtP \ (AAtilde' * XX0tilde + CCtilde' * Ytilde);
 
 Zdraw        = randn(rndStream, Nx * T, Ndraws);
 Xdraw        = (sqrtP') \ (sqrtPXhat + Zdraw);
-Xdraw        = reshape(Xdraw, Nx, T, Ndraws);
 
 if nargout > 1
+    Xshock = AA * Xdraw;
+    Xshock = reshape(Xshock, Nx, T, Ndraws);
+end
+
+Xdraw        = reshape(Xdraw, Nx, T, Ndraws);
+
+if nargout > 2
     Xhat        = (sqrtP') \ sqrtPXhat;
     Xhat        = reshape(Xhat, Nx, T);
 end
