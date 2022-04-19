8ddb9fa7253838e8393d0c2ebbdcf3fa97698dfd
Elmar Mertens <em@elmarmertens.com>
2022-04-19 10:53:14 +0200

    rwnoisePrecSampler reset code to prev

--- emstatespace/rwnoisePrecisionBasedSampler.m
+++ emstatespace/rwnoisePrecisionBasedSampler.m
@@ -32,11 +32,15 @@ AA     = sparse(rowndx, colndx, values);
 
 CC     = cat(2, sparse(NyT, Nx), speye(NyT, NxT));
 
-if size(volSTATE, 2) == 1 % convernt volSTATE from Nx x 1 to Nx x T
-    volSTATE = repmat(volSTATE, 1, T);
-end
-volSTATE    = cat(1, sqrtV0, volSTATE(:));
-sqrtSIGMA   = sparse(1:NxTp1, 1:NxTp1, volSTATE);
+IT          = speye(T);
+sqrtSIGMA   = blkdiag(sqrtV0,  kron(IT, volSTATE));
+% todo: handle 3D and 2D cases
+% if size(volSTATE, 2) == 1 % convert volSTATE from Nx x 1 to Nx x T
+%     volSTATE = repmat(volSTATE, 1, T);
+% end
+% volSTATE    = cat(1, sqrtV0, volSTATE(:));
+% sqrtSIGMA   = sparse(1:NxTp1, 1:NxTp1, volSTATE);
+
 sqrtOMEGA   = sparse(1:NyT, 1:NyT, volNOISE(:)');
 
 %% set up  stacked system
