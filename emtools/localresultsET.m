6085a3d982fde497ea97f8666b6886e453fb0169
Elmar Mertens <em@elmarmertens.com>
2022-11-28 18:35:57 +0100

    added localresultsET script
    Signed-off-by: Elmar Mertens <em@elmarmertens.com>

---
+++ emtools/localresultsET.m
@@ -0,0 +1,10 @@
+function p = localresultsET
+% localresultsMCMC designates a folder where matlab outputs will be stored
+% default: tmp folder in current working directory
+
+p = fullfile(pwd, 'tmp');
+
+
+if ~exist(p, 'dir')
+    mkdir(p);
+end
