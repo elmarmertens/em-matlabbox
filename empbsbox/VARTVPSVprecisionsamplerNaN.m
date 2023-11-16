050ae1544d1e5b975ab918705f0c7a787f1a5a02
Elmar Mertens <em@elmarmertens.com>
2023-11-16 12:40:19 +0100

    improved missing-data VAR preciions-based sampler (and renamed)
    Signed-off-by: Elmar Mertens <em@elmarmertens.com>

--- empbsbox/VARTVPSVprecisionsamplerNaN.m
+++ empbsbox/VARTVPSVprecisionsamplerNaN.m
@@ -1,27 +1,42 @@
 function [Xdraw, CC, QQ, RR1, arows, acols, asortndx, brows, bcols, bsortndx] = ...
-    VARTVPSVprecisionsamplerNaN0const(aaa,invbbb,ccc,y,yNaN,x0,xbar,rndStream,CC,QQ,RR1,...
-    arows, acols, asortndx, brows, bcols, bsortndx)
+    VARTVPSVprecisionsamplerNaN(aaa,invbbb,y,yNaN,y0,ybar,rndStream,CC,QQ,RR1,...
+    arows, acols, asortndx, brows, bcols, bsortndx, Ndraws)
 % VARTVPSVprecisionsamplerNaN0const for case of VAR(p) with missing values and fixed initial conditions
+%
+% USAGE: [Xdraw, CC, QQ, RR1, arows, acols, asortndx, brows, bcols, bsortndx] = ...
+%     VARTVPSVprecisionsamplerNaN(aaa,invbbb,ccc,y,yNaN,y0,ybar,rndStream,CC,QQ,RR1,...
+%     arows, acols, asortndx, brows, bcols, bsortndx)
 %
 % aaa is Ny x Ny x p (x T) (T dimension is optional)
 % invbbb is Ny x Ny (x T) (T dimension is optional); invbbb * invbbb' is inverse of variance of VAR residuals
 % ccc is measurement vector (typically be identity matrix), in general Ny x Ny ( x T)
-% x0 is Ny x p matrix of initial conditions (p lags of y)
+%
+% y is Ny x T data matrix, and yNaN is Ny x T logical matrix that designates the missing values 
+% values of y(yNaN) will be ignored
+%
+% y0 is Ny x p matrix of initial conditions (p lags of y)
+% ybar is a vector of intercepts (Ny x 1) or a matrix of time-varying intercepts
 % arguments after rndStream can be empty and will be returned as outputs for use in future calls
 % Xdraws is Ny * T vector output (can be shaped to Ny x T)
 
 %% VERSION INFO
 % AUTHOR    : Elmar Mertens
 
+if nargin < 17
+    Ndraws = 1;
+end
 
 % get dimensions
 [Ny, T] = size(y);
 p       = size(aaa,3);
-Nx      = size(x0,1);
+Ny0     = size(y0,1);
 Nw      = size(invbbb,2);
 
-if Nw ~= Nx
-    error('Expecting Nw identical to Nx')
+if Nw ~= Ny
+    error('Expecting Nw identical to Ny')
+end
+if Ny ~= Ny0
+    error('Expecting Ny identical to Ny0')
 end
 
 if nargin < 9
@@ -37,9 +52,8 @@ end
 if ismatrix(invbbb)
     invbbb = repmat(invbbb, [1 1 T]);
 end
-if ismatrix(ccc)
-    ccc = repmat(ccc, [1 1 T]);
-end
+
+Nx    = Ny; % for ease of comparison against more general state space routines
 
 NyT   = Ny * T;
 NwT   = Nw * T;
@@ -55,17 +69,20 @@ Y     = Y(~Ynan);
 NxNx         = Nx * Nx;
 NxNxT        = NxNx * T;
 invbbb       = reshape(invbbb, NxNxT, 1);
-ccc          = reshape(ccc, Ny * NxT, 1);
 
-XX0          = repmat(xbar, T, 1);
+if isvector(ybar) && (length(ybar) ==  Ny)
+    XX0  = repmat(ybar, T, 1);
+else
+    XX0  = ybar(:); 
+end
 
 %% adjust XX0 for initial conditions
 % adjust for initial conditions
-for k = 1 : p
+for k = 1 : min(p,T)
     xndx                       = (k-1) * Ny + (1 : Ny);
     theseInitialLags           = p - k + 1;
     thisA                      = reshape(aaa(:,:,k:p), Ny, Ny * theseInitialLags);
-    thisX0                     = reshape(x0(:,1:p-k+1), Ny * theseInitialLags, 1);
+    thisX0                     = reshape(y0(:,1:p-k+1), Ny * theseInitialLags, 1);
     XX0(xndx)                  = XX0(xndx) + thisA * thisX0; 
 end
 
@@ -109,21 +126,7 @@ if isempty(CC)
     bcols         = bcols(bsortndx);
 
     % C
-    crows     = repmat((1 : Ny)', 1 , Nx, T);
-    crows     = crows + permute(Ny * (0 : T-1), [1 3 2]);
-    crows     = crows(:);
-    ccols     = repmat(1 : NxT, Ny, 1);
-    ccols     = ccols(:);
-
-
-    % sort C indices
-    ndx = sub2ind([NyT, NxT], crows, ccols);
-    [~, csortndx] = sort(ndx);
-    crows         = crows(csortndx);
-    ccols         = ccols(csortndx);
-    ccc           = ccc(csortndx);
-    CC            = sparse(crows, ccols, ccc, NyT, NxT);
-
+    CC        = speye(NyT);
     % drop rows associated with NaN
     CC        = CC(~yNaN,:);
     % perform QR
@@ -144,11 +147,12 @@ QQ2       = QQ(:,N1+1:end)';
 
 %% sparse builds for BB and AA
 % AA
-Naa              = NxT + NxNx * p * (T - p) +sum(NxNx * (1 : p - 1));
+pEff             = min(T,p); % to catch cases where T<p
+Naa              = NxT + NxNx * p * max(T - p, 0) + sum(NxNx * (1 : pEff - 1));
 values           = ones(Naa,1);
 % fill p lags (sequentially)
 offset = NxT;
-for k = 1 : p
+for k = 1 : pEff
     values(offset + (1 : NxNx * (T-k))) = -reshape(aaa(:,:,k,1+k:T), Nx * Nx * (T - k), 1);
     offset = offset + NxNx * (T-k);
 end
@@ -176,7 +180,7 @@ cholinvQSIG22 = chol(invQSIG22, 'lower');
 
 X2hat         = - cholinvQSIG22 \ (AAtildeQQ2' * AAtildeQQX1);
 
-Z2draw        = randn(rndStream, N2, 1) + X2hat;
+Z2draw        = randn(rndStream, N2, Ndraws) + X2hat;
 X2draw        = cholinvQSIG22' \ Z2draw;
 Xdraw         = EX + QQX1tilde + QQ2' * X2draw;
 
