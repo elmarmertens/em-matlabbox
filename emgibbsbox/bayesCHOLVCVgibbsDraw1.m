b43f20905c05cdc99f0943dc15f87cffb1ac1fe9
Elmar Mertens <em@elmarmertens.com>
2024-01-03 12:40:34 +0100

    new: bayesCHOLVCV enforces choleski
    Signed-off-by: Elmar Mertens <em@elmarmertens.com>

---
+++ emgibbsbox/bayesCHOLVCVgibbsDraw1.m
@@ -0,0 +1,60 @@
+function [iwishcholDraw, cholSigmaT, dof] = bayesCHOLVCVgibbsDraw1(Sigma0T, dof0, resid, rndStream)
+% bayesSQRTVCVgibbsDraw1 iwishcholDraws CHOLESKI factor of variance-covariance matrix
+% [iwishcholDraws, cholSigmaT, dof] = bayesCHOLVCVgibbsDraw1(Sigma0T, dof0, resid, rndStream)
+% Prior is (Sigma0T, dof0) invWishart and posterior is (SigmaT, dof) invWishart
+% Recall: Mean of inverse Wishart is SigmaT / (dof - N - 1)
+% dof = dof0 + T
+%
+% resid is T x N
+%
+% same as bayesSQRTVCVgibbsDraw1, but enforces lower triangular choleski factorization
+%
+%
+% See also iwishdraw, bayesVCVgibbsDraw1, bayesVCVgibbsDraw, bayesSQRTVCVgibbsDraw1
+
+%   Coded by  Elmar Mertens, em@elmarmertens.com
+
+
+%% parse inputs
+if nargin < 4 || isempty(rndStream)
+    rndStream = getDefaultStream;
+end
+
+
+if ~isscalar(dof0)
+    error('dof must be a scalar')
+end
+
+%% draw random normals
+if isnumeric(rndStream)
+    z = rndStream;
+else
+    [T, Ny] = size(resid);
+    dof     = dof0 + T;
+    z       = randn(rndStream, Ny, dof);
+end
+
+%% Posterior Update
+% Sigma0T     = cholSigma0T * cholSigma0T';
+SigmaT      = Sigma0T + resid' * resid;
+cholSigmaT  = chol(SigmaT)';
+
+% alt QR code: (way slower)
+% [~,sqrtSigmaT] = qr([cholSigma0T'; resid], 0);
+% sqrtSigmaT     = sqrtSigmaT';
+% checkdiff(SigmaT, sqrtSigmaT * sqrtSigmaT');
+
+
+%% compute invWishart iwishDraws from iW(Sigma, dof)
+ZZ             = z * transpose(z);
+sqrtZZ         = chol(ZZ); % note absence of transpose
+iwishcholDraw  = cholSigmaT / sqrtZZ;
+
+% enforce choleski:
+iwishcholDraw = chol(iwishcholDraw * transpose(iwishcholDraw), 'lower');
+
+% slower:
+% wishdraw       = cholSigmaT / ZZ * cholSigmaT';
+% iwishcholDraw2 = chol(wishdraw, 'lower');
+% checkdiff(iwishcholDraw, iwishcholDraw2);
+
