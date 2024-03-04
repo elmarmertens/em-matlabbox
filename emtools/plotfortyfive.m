432c46971e1e4450d0a1117a1f9f22fe159b4002
Elmar Mertens <em@elmarmertens.com>
2024-03-04 22:15:09 +0100

    add: plotfortyfive takes varargin

--- emtools/plotfortyfive.m
+++ emtools/plotfortyfive.m
@@ -1,14 +1,17 @@
-function plotfortyfive()
-% PLOTFORTYFIVE ... 
-%  
-%   ... 
+function plotfortyfive(varargin)
+% PLOTFORTYFIVE ...
+%
+%   ...
 
-%% VERSION INFO 
-% AUTHOR    : Elmar Mertens 
-% $DATE     : 18-Jul-2021 22:34:15 $ 
-% $Revision : 1.00 $ 
-% DEVELOPED : 9.10.0.1684407 (R2021a) Update 3 
-% FILENAME  : plotfortyfive.m 
+%% VERSION INFO
+% AUTHOR    : Elmar Mertens
+% $DATE     : 18-Jul-2021 22:34:15 $
+% $Revision : 1.00 $
+% DEVELOPED : 9.10.0.1684407 (R2021a) Update 3
+% FILENAME  : plotfortyfive.m
 
-
-plot(xlim, xlim, 'k--')
+if isempty(varargin)
+    plot(xlim, xlim, 'k--')
+else
+    plot(xlim, xlim, varargin{:})
+end
