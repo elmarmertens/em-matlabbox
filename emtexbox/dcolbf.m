c571f220b4de65821e6f68fe2dd4a0e30228b9b8
Elmar Mertens <em@elmarmertens.com>
2024-03-24 10:43:30 +0100

    update and drop: dcol functions
    Signed-off-by: Elmar Mertens <em@elmarmertens.com>

--- emtexbox/dcolbf.m
+++ emtexbox/dcolbf.m
@@ -1,27 +1,40 @@
-function out = dcolbf(x, fmt, framenum)
-% BOLDCOLSTAR ... 
-%  
-%   ... 
+function out = dcolbf(x, fmt, boldflag, framenum)
+% BOLDCOLSTAR ...
+%
+%   ...
 
-%% VERSION INFO 
-% AUTHOR    : Elmar Mertens 
-% $DATE     : 09-Sep-2020 20:25:04 $ 
-% $Revision : 1.00 $ 
-% DEVELOPED : 9.8.0.1451342 (R2020a) Update 5 
-% FILENAME  : boldcolstar.m 
+%% VERSION INFO
+% AUTHOR    : Elmar Mertens
+% $DATE     : 09-Sep-2020 20:25:04 $
+% $Revision : 1.00 $
+% DEVELOPED : 9.8.0.1451342 (R2020a) Update 5
+% FILENAME  : boldcolstar.m
 
 if nargin < 2 || isempty(fmt)
     fmt = '%6.4f';
 end
-if nargin < 3
+if nargin < 3 || isempty(boldflag)
+    dobold = false;
+else
+    if isa(boldflag,'function_handle')
+        dobold = boldflag(x);
+    else
+        dobold = boldflag;
+    end
+end
+if nargin < 4
     framenum = [];
 end
 fmtstr = sprintf(fmt, x);
-[pre, post] = strtok(fmtstr, '.');
-if isempty(framenum)
-    out = sprintf('{\\textbf{%s}}.{\\textbf{%s}}', pre, post(2:end));
+if dobold
+    [pre, post] = strtok(fmtstr, '.');
+    if isempty(framenum)
+        out = sprintf('{\\textbf{%s}}.{\\textbf{%s}}', pre, post(2:end));
+    else
+        out = sprintf('{\\textbf<%d>{%s}}.{\\textbf<%d>{%s}}', framenum, pre, framenum, post(2:end));
+    end
 else
-    out = sprintf('{\\textbf<%d>{%s}}.{\\textbf<%d>{%s}}', framenum, pre, framenum, post(2:end));
+    out = fmtstr;
 end
 
 
