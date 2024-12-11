b83e918281a18b0fe0063d43faac17100cf4b578
Elmar Mertens <em@elmarmertens.com>
2024-12-11 23:58:18 +0100

    update StochVolKSCAR1
    Signed-off-by: Elmar Mertens <em@elmarmertens.com>

--- emgibbsbox/StochVolKSCcorrsqrtAR1.m
+++ emgibbsbox/StochVolKSCcorrsqrtAR1.m
@@ -36,24 +36,6 @@ kai2States  = sum(rand(rndStream, Nsv, T) > cdf, 3) + 1;
 
 %% KSC State Space
 obs       = logy2 - KSC.mean(kai2States);
-zerosNsv  = zeros(Nsv);
-Insv      = eye(Nsv);
-A     = [diag(rho) zerosNsv; zerosNsv Insv];
-B     = [hVCVsqrt; zerosNsv];
-C     = [Insv Insv];
-sqrtR = zeros(Nsv,Nsv,T);
-for n = 1 : Nsv
-    sqrtR(n,n,:) = KSC.vol(kai2States(n,:));
-end
+noisevol  = KSC.vol(kai2States);
 
-
-x0           = [zeros(Nsv, 1); Eh0];
-sqrtVhtilde  = eye(Nsv); % Note: need fixed prior, not dependent on estimated rhos (alt: use prior rho)
-sqrtVx0      = [sqrtVhtilde, zerosNsv; zerosNsv sqrtVh0];
-[H, Hshock, H0] = a2b2c2DisturbanceSmoothingSampler1draw(A, B, C, obs, x0, sqrtVx0, ...
-    sqrtR, rndStream); 
-
-h      = H(1:Nsv,:) + H(Nsv+1:end,:); % C * H
-hbar   = H0(Nsv+1:end);
-htilde = cat(2, H0(1:Nsv,:), H(1:Nsv,:)); % demeaned component, including lagged value
-hshock = Hshock(1:Nsv,:);
+[h, hbar, hshock, htilde] = sampleVAR1noisePBS(obs, Nsv, T, rho, hVCVsqrt, Eh0, sqrtVh0, noisevol, rndStream);
