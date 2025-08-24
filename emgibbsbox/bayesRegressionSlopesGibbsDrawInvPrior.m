c10f78011f3ab4b76534f97c414e4bcdeee6283e
Elmar Mertens <em@elmarmertens.com>
2025-08-24 18:02:55 +0200

    Adds Gibbs sampler for Bayesian regression slopes
    Introduces a function that draws regression slopes conditional on precision using Gibbs sampling, assuming an inverse prior. This function allows for Bayesian inference in linear regression models.
    
    The implementation includes handling diffuse priors and provides an optional argument for a random number stream. It also calculates residuals if requested.

---
+++ emgibbsbox/bayesRegressionSlopesGibbsDrawInvPrior.m
@@ -0,0 +1,56 @@
+function [bdraw, resid] = bayesRegressionSlopesGibbsDrawInvPrior(y, X, h, iV0b0, iV0, Ndraws, rndStream)
+% BAYESREGRESSIONSLOPESGIBBSDRAW performs Gibbs steps for linear regression model 
+%   this function draws slopes conditional on precision (inverse residual variance)
+%   Usage: [bdraw, resid] =  bayesRegressionSlopesGibbsDrawInvPrior(y, X, h, iV0b0, iV0, Ndraws, rndStream)
+%
+% See also bayesRegressionGibbsDraw 
+
+%   Coded by  Elmar Mertens, em@elmarmertens.com
+
+%% VERSION INFO
+% AUTHOR    : Elmar Mertens
+% $DATE     : 17-Jan-2009 16:30:05 $
+% $Revision : 1.00 $
+% DEVELOPED : 7.7.0.471 (R2008b)
+% FILENAME  : bayesRegressionGibbsDraw.m
+
+if nargin < 6 || isempty(Ndraws)
+   Ndraws = 1;
+end
+if nargin < 7 || isempty(rndStream)
+   rndStream = getDefaultStream;
+end
+
+%% Some prelim preparations
+[T, K]      = size(X); %#ok<ASGLU>
+I           = eye(K);
+
+if nargin < 4 || isempty(iV0b0)
+    iV0b0 = zeros(K, 1);
+end
+if nargin < 5 || isempty(iV0) % diffuse prior
+    iV0 = zeros(K);
+end
+if nargin < 6 || isempty(Ndraws)
+   Ndraws = 1;
+end
+
+XX    = X' * X;
+Xy    = X' * y;
+
+% posterior precision
+iV     = iV0 + XX * h; 
+sqrtiV = chol(iV); % upper triangular
+
+%% draw regression slope
+zdraw  = randn(rndStream, K, Ndraws);
+
+bdraw     = sqrtiV' \ (iV0b0 + Xy * h); % preparatory step
+bdraw     = sqrtiV \ (bdraw + zdraw); 
+
+if nargout > 1
+    resid       = y - X * bdraw;
+end
+
+
+   
